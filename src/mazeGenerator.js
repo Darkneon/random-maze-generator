c430b7c45ec8e3ee61a9c29444623ae6988e070d
Robert Jakubowicz <robert@robert-jakubowicz.com>
2013-01-23 02:22:03 -0500

    Replaced tabs by spaces

--- src/mazeGenerator.js
+++ src/mazeGenerator.js
@@ -1,24 +1,24 @@
 var MazeGenerator = function(rows, cols) {
-	this.graph = new Graph(rows, cols);
-	this.cellStack = [];
-    this.path      = [];
+  this.graph = new Graph(rows, cols);
+  this.cellStack = [];
+  this.path      = [];
 
-	var self = this;
+  var self = this;
 
-	var recurse = function(cell) {
-		cell.visit();
+  var recurse = function(cell) {
+    cell.visit();
     var neighbors = self.graph.cellUnvisitedNeighbors(cell);
     if(neighbors.length > 0) {
-    	var randomNeighbor = neighbors[Math.floor(Math.random() * neighbors.length)];
-    	self.cellStack.push(cell);
-    	self.graph.removeEdgeBetween(cell, randomNeighbor);
-    	recurse(randomNeighbor);
+      var randomNeighbor = neighbors[Math.floor(Math.random() * neighbors.length)];
+      self.cellStack.push(cell);
+      self.graph.removeEdgeBetween(cell, randomNeighbor);
+      recurse(randomNeighbor);
     }
     else {
-    	var waitingCell = self.cellStack.pop();
-    	if(waitingCell) {
-    		recurse(waitingCell);
-    	}
+      var waitingCell = self.cellStack.pop();
+      if(waitingCell) {
+        recurse(waitingCell);
+      }
     }
   };
  
@@ -35,7 +35,7 @@ var MazeGenerator = function(rows, cols) {
   };
 
   this.generate = function() {
-	var initialCell = this.graph.getCellAt(0, 0);
-	recurse(initialCell);
+    var initialCell = this.graph.getCellAt(0, 0);
+    recurse(initialCell);
   };
 };
